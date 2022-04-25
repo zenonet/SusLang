ff900f71b94ea24a568ea689ce622d495ef9c222
zenonet <78077158+zenonet@users.noreply.github.com>
2022-04-25 19:35:25 +0200

    Removed unused using statement

--- SusLang/Program.cs
+++ SusLang/Program.cs
@@ -1,10 +1,8 @@
 ﻿
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 public static class Program
 {
