362982f7abb7e7c9c5cc908562dedfe14b980b00
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-02 18:52:06 +0200

    Added loops like in brainfuck

---
+++ SusLangCompiler/ParsingUtility.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SusLang
+{
+    internal static class ParsingUtility
+    {
+        public static string FindBetweenBrackets(ref string code)
+        {
+            
+            //Removes the first bracket if it is inside of code
+            if (code[0] == '[')
+            {
+                code = code.Substring(1);
+            }
+            
+            int openedBraces = 1;
+
+            int relaxIndex = 0;
+            //Tracks all brackets until it finds the matching one
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (code[i] == '[')
+                    openedBraces++;
+
+                if(code[i] == ']')
+                    openedBraces--;
+
+                if (openedBraces != 0) continue;
+                
+                relaxIndex = i;
+                break;
+            }
+
+            List<char> list = code.ToList();
+            list.RemoveAt(relaxIndex);
+            code = new string(list.ToArray());
+            
+            string ret = relaxIndex == 0 ? null : code[..relaxIndex];
+            code = code[relaxIndex..];
+            return ret;
+        }
+    }
+}
