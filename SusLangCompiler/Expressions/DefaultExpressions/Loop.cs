44641954cda90ee39c910f507fa86f536cc90998
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-11 19:28:02 +0200

    Implemented loops into the new Parser

--- SusLangCompiler/Expressions/DefaultExpressions/Loop.cs
+++ SusLangCompiler/Expressions/DefaultExpressions/Loop.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+
 namespace SusLang.Expressions.DefaultExpressions
 {
     public class Loop : Expression
     {
-        //TODO: Logic go here
+        private readonly Queue<Expression> _expressions = new();
+        protected override bool OnParse(ref string code)
+        {
+            string inside = ParsingUtility.FindBetweenBrackets(ref code);
+            while (inside.Length > 0)
+            {
+                Expression expression = Expression.Parse(inside, out inside);
+                if (expression is null)
+                    return false;
+                
+                _expressions.Enqueue(expression);
+            }
+
+            return true;
+
+        }
+
+        public override void Execute()
+        {
+            while (Compiler.Crewmates[Compiler.SussedColor] > 0)
+            {
+                foreach (Expression expression in _expressions) expression.Execute();
+            }
+        }
     }
 }
