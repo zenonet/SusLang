e92121cc86b8ebecdee4a1777f2eab222102cba6
zenonet <78077158+zenonet@users.noreply.github.com>
2022-08-27 23:27:48 +0200

    Diagnoses are now auto added to Context.Diagnoses

--- SusLang.Compiler/CodeAnalysis/Diagnosis.cs
+++ SusLang.Compiler/CodeAnalysis/Diagnosis.cs
@@ -1,23 +1,29 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace SusLang.CodeAnalysis;
 
 public readonly struct Diagnosis
 {
-    public Diagnosis(string message, InspectionSeverity severity, int lineNumber, int columnNumber = -1)
+    public Diagnosis(ExecutionContext context, string message, InspectionSeverity severity, int lineNumber, int columnNumber = -1)
     {
         Message = message;
         Severity = severity;
         LineNumber = lineNumber;
+        Context = context;
         ColumnNumber = columnNumber;
+
+        context.Diagnoses.Add(this);
     }
-    
+
+    [JsonIgnore]
+    public ExecutionContext Context { get; }
     public int LineNumber { get; }
     public int ColumnNumber { get; }
-    
+
     public string Message { get; }
     public InspectionSeverity Severity { get; }
-    
+
     public string GetJson()
     {
         return JsonSerializer.Serialize(this);
