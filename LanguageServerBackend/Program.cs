10731d9659f0c161f975ff75d951c9d4c9d7707a
zenonet <78077158+zenonet@users.noreply.github.com>
2022-08-27 23:29:33 +0200

    Added a new project that only returns Diagnoses of a script as json

---
+++ LanguageServerBackend/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using ExecutionContext = SusLang.ExecutionContext;
+
+string arg = args[0];
+
+if (File.Exists(arg))
+{
+    ExecutionContext context = SusLang.Compiler.CreateAst(File.ReadAllText(arg), true);
+
+    string diagnoses = JsonSerializer.Serialize(context.Diagnoses);
+
+    Console.WriteLine(diagnoses);
+}
