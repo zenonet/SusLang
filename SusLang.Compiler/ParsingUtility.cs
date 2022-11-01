d22ca69c33743dda5a6b128666fd9cbb26553c00
zenonet <zeno.wilke@wgmail.de>
2022-11-01 19:00:45 +0100

    Fixed ParsingUtility.FindBetweenBrackets() breaking when the code only contained the opening bracket itself

--- SusLang.Compiler/ParsingUtility.cs
+++ SusLang.Compiler/ParsingUtility.cs
@@ -3,17 +3,16 @@ using System.Linq;
 
 namespace SusLang
 {
-    internal static class ParsingUtility
+    public static class ParsingUtility
     {
         public static string FindBetweenBrackets(ref string code)
         {
-            
             //Removes the first bracket if it is inside of code
             if (code[0] == '[')
             {
                 code = code.Substring(1);
             }
-            
+
             int openedBraces = 1;
 
             int relaxIndex = 0;
@@ -23,20 +22,23 @@ namespace SusLang
                 if (code[i] == '[')
                     openedBraces++;
 
-                if(code[i] == ']')
+                if (code[i] == ']')
                     openedBraces--;
 
                 if (openedBraces != 0) continue;
-                
+
                 relaxIndex = i;
                 break;
             }
 
-            //Removes the bracket itself from the string
-            List<char> list = code.ToList();
-            list.RemoveAt(relaxIndex);
-            code = new string(list.ToArray());
-            
+            if (relaxIndex != 0)
+            {
+                //Removes the bracket itself from the string
+                List<char> list = code.ToList();
+                list.RemoveAt(relaxIndex);
+                code = new(list.ToArray());
+            }
+
             //Return everything before relaxIndex. If relaxIndex is 0 (no closing bracket was found) return null
             string ret = relaxIndex == 0 ? null : code[..relaxIndex];
             code = code[relaxIndex..];
