aecb9c530d431fe1a1709b484e694c527a52032f
zenonet <zeno.wilke@wgmail.de>
2022-11-02 14:19:07 +0100

    Refactoring

--- SusLang.Compiler/Crewmate.cs
+++ SusLang.Compiler/Crewmate.cs
@@ -1,66 +1,62 @@
 using System.Linq;
-using SoftCircuits.Collections;
-using SusLang.Expressions.DefaultExpressions;
 
 namespace SusLang;
 
-    public class Crewmate
+public sealed class Crewmate
+{
+    public string Name { get; }
+
+    public static readonly Crewmate RefToSelectedInstance = new();
+
+    public bool RefToSelected => ReferenceEquals(this, RefToSelectedInstance);
+
+    private Crewmate(string name)
     {
-        public const Crewmate Null = null;
-
-        public virtual string Name { get; }
-
-        public static readonly Crewmate RefToSelectedInstance = new();
-        
-        public bool RefToSelected => ReferenceEquals(this, RefToSelectedInstance);
-
-        private Crewmate(string name)
-        {
-            Name = name;
-        }
-
-        protected Crewmate()
-        {
-        }
-
-
-        //Parse color
-        public static Crewmate Parse(string color, ExecutionContext context)
-        {
-            Crewmate parsedCrewmate = context.Crewmates.FirstOrDefault(
-                x => x.Key.Name == color
-            ).Key;
-
-            return parsedCrewmate;
-        }
-
-        public override string ToString()
-        {
-            return "Crewmate: " + Name;
-        }
-
-        public static implicit operator Crewmate(string color)
-        {
-            return new (color);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is Crewmate crewmate)
-            {
-                return Name.Equals(crewmate.Name);
-            }
-            
-            return obj == this;
-        }
-
-        protected bool Equals(Crewmate other)
-        {
-            return Name == other.Name;
-        }
-
-        public override int GetHashCode()
-        {
-            return Name != null ? Name.GetHashCode() : 0;
-        }
+        Name = name;
     }
+
+    private Crewmate()
+    {
+    }
+
+
+    //Parse color
+    public static Crewmate Parse(string color, ExecutionContext context)
+    {
+        Crewmate parsedCrewmate = context.Crewmates.FirstOrDefault(
+            x => x.Key.Name == color
+        ).Key;
+
+        return parsedCrewmate;
+    }
+
+    public override string ToString()
+    {
+        return "Crewmate: " + Name;
+    }
+
+    public static implicit operator Crewmate(string color)
+    {
+        return new(color);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Crewmate crewmate)
+        {
+            return Name.Equals(crewmate.Name);
+        }
+
+        return obj == this;
+    }
+
+    private bool Equals(Crewmate other)
+    {
+        return Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return Name != null ? Name.GetHashCode() : 0;
+    }
+}
